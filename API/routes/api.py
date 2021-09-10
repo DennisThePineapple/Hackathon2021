51bba06bed60aeebdaa0d66fd25036e0b9a787bb
brianriwukaho <b.riwukaho@gmail.com>
2021-09-10 23:57:31 +1000

    initial submit and leaderboard endpoints

--- API/routes/api.py
+++ API/routes/api.py
@@ -1,7 +1,27 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI, File, Form, UploadFile
 
 router = APIRouter(prefix='/api', tags=['api'])
 
-@router.post('/')
-def suck_my_cock():
-    return "Ah yeah baby that feels good"
+from pydantic import BaseModel
+
+# Firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account
+credentials = credentials.Certificate('./auth.json')
+firebase_admin.initialize_app(credentials)
+
+db = firestore.client()
+
+@router.post('/submit')
+async def test( file: UploadFile = File(...), userId: str = Form(...)):
+    return {
+        "file_size": len(file),
+        "userdId": userId
+    }
+
+@router.get('/leaderboards')
+async def test():
+    return "leaderboards endpoint"
