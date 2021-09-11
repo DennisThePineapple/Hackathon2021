9b51b3de08a42086212cfbf297dfc66b931f42c7
brianriwukaho <b.riwukaho@gmail.com>
2021-09-11 12:03:27 +1000

    filled out submit api

--- API/routes/api.py
+++ API/routes/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, FastAPI, File, Form, UploadFile
 
-router = APIRouter(prefix='/api', tags=['api'])
+import datetime
 
 from pydantic import BaseModel
 
@@ -9,19 +9,45 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+router = APIRouter(prefix='/api', tags=['api'])
+
 # Use a service account
 credentials = credentials.Certificate('./auth.json')
 firebase_admin.initialize_app(credentials)
 
 db = firestore.client()
 
+# receive a photo and userId
+# compute recyclable materials and points in pytorch model
+# store results in db and send down points breakdown objects
+
+
 @router.post('/submit')
-async def test( file: UploadFile = File(...), userId: str = Form(...)):
-    return {
-        "file_size": len(file),
-        "userdId": userId
+async def test(file: UploadFile = File(...), userId: str = Form(...)):
+    # replace RHS with real model inference invocation e.g. `materials = InferMaterials(file)``
+    materials = {
+        'plastic': 1,
+        'paper': 2,
+        'metal': 3
     }
 
-@router.get('/leaderboards')
+    date = datetime.datetime.now()
+
+    item_data = dict()
+    item_data['userId'] = userId
+    item_data['date'] = date
+
+    for item in materials.items():
+        (material, points) = item
+        item_data['material'] = material
+        item_data['points'] = points
+
+        # db.collection(...).add(...) auto-generates a unique id for the insert
+        await db.collection("items").add(item_data)
+
+    return materials
+
+
+@ router.get('/leaderboards')
 async def test():
     return "leaderboards endpoint"
